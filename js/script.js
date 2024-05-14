8a86126c49390a0378c514ae9247d9adf4d954ae
Maria Eduarda <mariaeduardabotelho24@gmail.com>
2024-05-13 22:04:58 -0300

    js

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// TESTE !!!
+
+
+// MENU HAMBURGUER
+
+function mudouTamanho(){
+    if(window.innerWidth >= 700){
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
+    }
+}
+function clickMenu(){
+    if (itens.style.display == 'block'){
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+    }
+
+}
+
+
+
+
+// QUE PORRA É ESSA 
+///let opcao = confirm('Você é CORNO?');
+//if(opcao==true){
+  //  window.location.href='https://www.youtube.com/watch?v=08k0zv_e9yQ';
+//}else{window.location.href='https://www.youtube.com/shorts/tYIQuk_UEM8';}
+
+
